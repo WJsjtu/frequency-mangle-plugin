ce68ba5d0d8f0a927e24fb669972ea3796a98ed4
Jason Wang <JasonWang0421@gmail.com>
2017-02-06 12:07:18 +0800

    inti code

---
+++ README.md
@@ -0,0 +1,5 @@
+# frequency-mangle-plugin
+A property/string mangle plugin for Webpack. Webpack plugin for [frequency-mangle](https://github.com/WJsjtu/frequency-mangle).
+
+###Install
+`npm install frequency-mangle-plugin`
---
+++ index.js
@@ -0,0 +1,39 @@
+var RawSource = require("webpack-sources").RawSource;
+var ModuleFilenameHelpers = require("webpack/lib/ModuleFilenameHelpers");
+var FrequencyMangle = require("frequency-mangle");
+
+var FrequencyManglePlugin = function (options) {
+    this.options = options;
+};
+
+FrequencyManglePlugin.prototype.apply = function (compiler) {
+    var options = this.options;
+    options.test = options.test || /\.js$/i;
+
+    compiler.plugin("compilation", function (compilation) {
+        compilation.plugin("optimize-chunk-assets", function (chunks, callback) {
+
+            var files = [];
+            chunks.forEach(function (chunk) {
+                files.push.apply(files, chunk.files);
+            });
+            files.push.apply(files, compilation.additionalChunkAssets);
+            files = files.filter(ModuleFilenameHelpers.matchObject.bind(undefined, options));
+
+            files.forEach(function (file) {
+                var asset = compilation.assets[file];
+                var input = asset.source();
+
+                var output = FrequencyMangle.mangle(input, options);
+
+                if (output) {
+                    compilation.assets[file] = new RawSource(output);
+                }
+            });
+
+            callback();
+        });
+    });
+};
+
+module.exports = FrequencyManglePlugin;
